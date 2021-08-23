6d45e4aa388f4d468eacb505bf9c5a1b3bab63af
Phan Đại Phước <pdphuoc7072@gmail.com>
2021-08-23 19:27:01 +0700

    update lai bai 11

--- module2/src/ss11_dsa_stack_queue/bai_tap_optional/to_chuc_du_lieu_hop_ly_demerging_su_dung_queue/DemergingQueue.java
+++ module2/src/ss11_dsa_stack_queue/bai_tap_optional/to_chuc_du_lieu_hop_ly_demerging_su_dung_queue/DemergingQueue.java
@@ -1,6 +1,8 @@
 package ss11_dsa_stack_queue.bai_tap_optional.to_chuc_du_lieu_hop_ly_demerging_su_dung_queue;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -12,17 +14,16 @@ public class DemergingQueue {
         Queue<Person> queueMale = new LinkedList<>();
         Queue<Person> queueFemale = new LinkedList<>();
         ArrayList<Person> listPerson = new ArrayList<>();
-        Person person1 = new Person("Phước", "male", new int[]{27, 7, 1990});
-        Person person2 = new Person("Hà", "female", new int[]{19, 8, 1992});
-        Person person3 = new Person("Kiệt", "male", new int[]{2, 3, 1989});
-        Person person4 = new Person("Na", "female", new int[]{8, 1, 1994});
-        Person person5 = new Person("Thủy", "female", new int[]{20, 5, 1988});
-        listPerson.add(person1);
-        listPerson.add(person2);
-        listPerson.add(person3);
-        listPerson.add(person4);
-        listPerson.add(person5);
-        
+        listPerson.add(new Person("An", "female", LocalDate.of(1985, 7, 27)));
+        listPerson.add(new Person("Nam", "male", LocalDate.of(1998, 8, 19)));
+        listPerson.add(new Person("Anh", "male", LocalDate.of(1989, 3, 12)));
+        listPerson.add(new Person("Bình", "male", LocalDate.of(1987, 1, 8)));
+        listPerson.add(new Person("Thủy", "female", LocalDate.of(1988, 2, 5)));
+        listPerson.add(new Person("Hiền", "female", LocalDate.of(1996, 12, 15)));
+        listPerson.sort(Comparator.comparing(list -> list.getDateOfBirth().getYear()));
+
+        System.out.println("Trước khi sắp xếp nữ trước nam, danh sách (đã sắp xếp theo ngày tháng năm sinh) là: \n" + listPerson);
+
         for (Person person : listPerson) {
             switch (person.getGender()) {
                 case MALE:
@@ -41,6 +42,7 @@ public class DemergingQueue {
         while (!queueMale.isEmpty()) {
             listAfterDemerging.add(queueMale.poll());
         }
-        System.out.println(listAfterDemerging);
+
+        System.out.println("Sau khi sắp xếp nữ trước nam, danh sách (vẫn giữ được tính sắp xếp theo ngày tháng năm sinh) sẽ là: \n" + listAfterDemerging);
     }
 }
--- module2/src/ss11_dsa_stack_queue/bai_tap_optional/to_chuc_du_lieu_hop_ly_demerging_su_dung_queue/Person.java
+++ module2/src/ss11_dsa_stack_queue/bai_tap_optional/to_chuc_du_lieu_hop_ly_demerging_su_dung_queue/Person.java
@@ -1,18 +1,19 @@
 package ss11_dsa_stack_queue.bai_tap_optional.to_chuc_du_lieu_hop_ly_demerging_su_dung_queue;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Person {
     private String name;
     private String gender;
-    private int[] dateOfBirth = new int[3];
+    private LocalDate dateOfBirth;
     static ArrayList<Person> listOfPerson = new ArrayList<>();
 
     public Person() {
     }
 
-    public Person (String name, String gender, int[] dateOfBirth) {
+    public Person (String name, String gender, LocalDate dateOfBirth) {
         this.name = name;
         this.gender = gender;
         this.dateOfBirth = dateOfBirth;
@@ -34,16 +35,16 @@ public class Person {
         this.gender = gender;
     }
 
-    public int[] getDateOfBirth() {
+    public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public void setDateOfBirth(int[] dateOfBirth) {
+    public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
     @Override
     public String toString () {
-        return " name = " + getName() + ", gender = " + getGender() + ", dateOfBirth = " + Arrays.toString(getDateOfBirth()) + "\n";
+        return " name = " + getName() + ", gender = " + getGender() + ", dateOfBirth = " + getDateOfBirth() + "\n";
     }
 }
