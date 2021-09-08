90fa2032c0bb1783927c10f05cf0f99e49f6082d
Phan Đại Phước <pdphuoc7072@gmail.com>
2021-09-08 17:03:18 +0700

    update

--- module2/src/ss15_xu_ly_ngoai_le_va_debug/bai_tap/su_dung_lop_illegal_triangle_exception/Test.java
+++ module2/src/ss15_xu_ly_ngoai_le_va_debug/bai_tap/su_dung_lop_illegal_triangle_exception/Test.java
@@ -5,12 +5,38 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Hãy nhập cạnh a của tam giác: ");
-        double sideAOfTriangle = Double.parseDouble(scanner.nextLine());
-        System.out.print("Hãy nhập cạnh b của tam giác: ");
-        double sideBOfTriangle = Double.parseDouble(scanner.nextLine());
-        System.out.print("Hãy nhập cạnh c của tam giác: ");
-        double sideCOfTriangle = Double.parseDouble(scanner.nextLine());
+        double sideAOfTriangle;
+        while (true) {
+            try {
+                System.out.print("Hãy nhập cạnh a của tam giác: ");
+                sideAOfTriangle = Double.parseDouble(scanner.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        double sideBOfTriangle;
+        while (true) {
+            try {
+                System.out.print("Hãy nhập cạnh b của tam giác: ");
+                sideBOfTriangle = Double.parseDouble(scanner.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        double sideCOfTriangle;
+        while (true) {
+            try {
+                System.out.print("Hãy nhập cạnh c của tam giác: ");
+                sideCOfTriangle = Double.parseDouble(scanner.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        }
         try {
             new Triangle(sideAOfTriangle, sideBOfTriangle, sideCOfTriangle);
             System.out.println("Đây là 3 cạnh của 1 tam giác");
